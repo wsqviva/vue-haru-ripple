47a514cbc0b0d0f9cbdaef820128df754c43af9c
wsqviva <wsq19850829@gmail.com>
2016-08-31 15:59:37 +0800

    add more example

--- example/index.js
+++ example/index.js
@@ -5,5 +5,8 @@ new Vue({
   el: 'body',
   components: { 
     'vue-haru-ripple': ripple 
+  },
+  data: {
+    randomColor: '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
   }
 });
