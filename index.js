ab50aff91d37802ccec330c05c3020ce61143bbf
wsqviva <wsq19850829@gmail.com>
2016-09-06 23:44:17 +0800

    m variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 new Vue({
   el: 'body',
   components: { 
-    'vue-haru-ripple': ripple 
+    'vue-haru-ripple': VueHaruRipple 
   },
   data: {
     randomColor: '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
