9ccb6aa9b6b67f1032c0e310adfa18f088610ec0
wsqviva <wsq19850829@gmail.com>
2016-09-07 10:47:55 +0800

    add eslint

--- src/ripple.vue
+++ src/ripple.vue
@@ -8,54 +8,54 @@
 </template>
 
 <script>
-  import Vue from 'vue';
-  import Raf from 'better-raf';
-  let raf = Raf();
-  let vueUtil = Vue.util;
-  let sqrt = Math.sqrt;
-  let round = Math.round;
-  const INITIAL_SCALE = 0.001;
-  const FINAL_SCALE = 1;
+  import Vue from 'vue'
+  import Raf from 'better-raf'
+  const raf = Raf()
+  const vueUtil = Vue.util
+  const sqrt = Math.sqrt
+  const round = Math.round
+  const INITIAL_SCALE = 0.001
+  const FINAL_SCALE = 1
   // used to handle downActions triggered within one animation frame time
-  const FRAME_CHECK_COUNT = 1;
+  const FRAME_CHECK_COUNT = 1
   export default {
     name: 'vue-haru-ripple',
 
     ready() {
-      vueUtil.on(this.$el, 'mousedown', vueUtil.bind(this.downAction, this), false);
-      vueUtil.on(this.$el, 'touchstart', vueUtil.bind(this.downAction, this), false);
-      vueUtil.on(this.$el, 'mouseup', vueUtil.bind(this.upAction, this), false);
-      vueUtil.on(this.$el, 'mouseleave', vueUtil.bind(this.upAction, this), false);
-      vueUtil.on(this.$el, 'blur', vueUtil.bind(this.upAction, this), false);
-      vueUtil.on(this.$el, 'touchend', vueUtil.bind(this.upAction, this), false);
+      vueUtil.on(this.$el, 'mousedown', vueUtil.bind(this.downAction, this), false)
+      vueUtil.on(this.$el, 'touchstart', vueUtil.bind(this.downAction, this), false)
+      vueUtil.on(this.$el, 'mouseup', vueUtil.bind(this.upAction, this), false)
+      vueUtil.on(this.$el, 'mouseleave', vueUtil.bind(this.upAction, this), false)
+      vueUtil.on(this.$el, 'blur', vueUtil.bind(this.upAction, this), false)
+      vueUtil.on(this.$el, 'touchend', vueUtil.bind(this.upAction, this), false)
 
-      this._frameCount = 0;
-      let boundingRect = this._boundingRect = this.$el.getBoundingClientRect();
-      this._rippleSize = round(sqrt(boundingRect.width * boundingRect.width + boundingRect.height * boundingRect.height) * 2) + 2;
+      this._frameCount = 0
+      let boundingRect = this._boundingRect = this.$el.getBoundingClientRect()
+      this._rippleSize = round(sqrt(boundingRect.width * boundingRect.width + boundingRect.height * boundingRect.height) * 2) + 2
 
       // not pass color prop
       if (!this.color) {
-        let color = this.$els.content.firstElementChild && window.getComputedStyle(this.$els.content.firstElementChild).color;
-        this.color = color || '#fff';
+        let color = this.$els.content.firstElementChild && window.getComputedStyle(this.$els.content.firstElementChild).color
+        this.color = color || '#fff'
       }
     },
 
     transitions: {
       spread: {
         beforeEnter() {
-          this.waveClass.animating = true;
+          this.waveClass.animating = true
         },
 
         enter() {
           this.$nextTick(() => {
-            this.setWaveStyle();
-          });
+            this.setWaveStyle()
+          })
         },
 
         afterEnter() {
           // leave, afterLeave hooks not bound
-          this.showWave = false;
-          this.$emit('disappear');
+          this.showWave = false
+          this.$emit('disappear')
         }
       }
     },
@@ -87,62 +87,62 @@
           height: '',
           transform: ''
         }
-      };
+      }
     },
 
     methods: {
       setWaveStyle() {
-        this.waveStyle.transform = `${this._rippleTranslate} scale(${FINAL_SCALE}, ${FINAL_SCALE})`;
+        this.waveStyle.transform = `${this._rippleTranslate} scale(${FINAL_SCALE}, ${FINAL_SCALE})`
       },
 
       frameCountCheck() {
         if (this._frameCount > 0) {
-          return false;
+          return false
         }
-        this._frameCount = FRAME_CHECK_COUNT;
-        return true;
+        this._frameCount = FRAME_CHECK_COUNT
+        return true
       },
 
       animFrameHandler() {
-        this._frameCount--;
-        this.showWave = true;
+        this._frameCount--
+        this.showWave = true
       },
 
       upAction(event) {
         if (event.type === 'touchend' || event.type === 'touchcancel') {
-          event.preventDefault();
+          event.preventDefault()
         }
-        this.waveStyle.opacity = 0;
+        this.waveStyle.opacity = 0
       },
 
       downAction(event) {
         if (!this.frameCountCheck()) {
-          return;
+          return
         }
 
         if (event.type === 'mousedown') {
           // only left mouse button
           if (event.button !== 0) {
-            return;
+            return
           }
         }
 
-        let boundingRect = this._boundingRect;
+        let boundingRect = this._boundingRect
 
         // mousedown or touchstart x, y
-        let downX = event.touches ? event.touches[0].pageX : event.clientX;
-        let downY = event.touches ? event.touches[0].pageY : event.clientY;
+        let downX = event.touches ? event.touches[0].pageX : event.clientX
+        let downY = event.touches ? event.touches[0].pageY : event.clientY
 
-        let rippleX = round(downX - boundingRect.left);
-        let rippleY = round(downY - boundingRect.top);
+        let rippleX = round(downX - boundingRect.left)
+        let rippleY = round(downY - boundingRect.top)
 
         // ripple max diameter
-        let rippleTranslate = this._rippleTranslate = `translate(-50%, -50%) translate(${rippleX}px, ${rippleY}px)`;
-        let rippleSize = this._rippleSize;
+        let rippleTranslate = this._rippleTranslate = `translate(-50%, -50%) translate(${rippleX}px, ${rippleY}px)`
+        let rippleSize = this._rippleSize
 
-        this.showWave = false;
+        this.showWave = false
         // prev ripple vanish immediately, so need class 'animating'
-        this.waveClass.animating = false;
+        this.waveClass.animating = false
 
         this.waveStyle = {
           opacity: this.opacity,
@@ -150,14 +150,14 @@
           width: `${rippleSize}px`,
           height: `${rippleSize}px`,
           transform: `${rippleTranslate} scale(${INITIAL_SCALE}, ${INITIAL_SCALE})`
-        };
+        }
 
         raf.requestAnimationFrame(() => {
-          this.animFrameHandler();
-        });
+          this.animFrameHandler()
+        })
       }
     }
-  };
+  }
 </script>
 
 <style lang="stylus" scoped>
